33a7fc4667d3ef98f80d709d773a494530d99c05
Jonathon Pemberton <jpemberton1@chewy.com>
2021-01-27 19:45:12 -0500

    Add CSV formatter

--- src/csv.js
+++ src/csv.js
@@ -1,2 +1,15 @@
-export default function csv() {
+export default function csv(json) {
+  let out = "";
+  for (let i = 0; i < json.length; i += 1) {
+    const row = json[i];
+    const keys = Object.keys(row);
+    for (let j = 0; j < keys.length; j += 1) {
+      out += row[keys[j]];
+      if (j < keys.length - 1)
+        out += ","
+    }
+    if (i < json.length - 1)
+      out += "\n";
+  }
+  return out;
 }
